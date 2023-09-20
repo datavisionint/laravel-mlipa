7b6d126ce753c7a1a5adf828946287deeb749570
mwakalingajohn <mwakalingajohn@users.noreply.github.com>
2023-09-20 08:58:42 +0000

    Fix styling

--- config/mlipa.php
+++ config/mlipa.php
@@ -5,33 +5,33 @@ return [
     /**
      * The client key from the mlipa developer portal
      */
-    "client_key" => env("MLIPA_CLIENT_KEY"),
+    'client_key' => env('MLIPA_CLIENT_KEY'),
 
     /**
      * The client secret from the mlipa developer portal
      */
-    "client_secret" => env("MLIPA_CLIENT_SECRET"),
+    'client_secret' => env('MLIPA_CLIENT_SECRET'),
 
     /**
      * Webhook route
      *
      * The route exposed in your application that will be used to receive webhooks
      */
-    "webhook_route" => "integrations/mlipa/webhook",
+    'webhook_route' => 'integrations/mlipa/webhook',
 
     /**
      * Webhook route name
      *
      * The route name used for webhook when using route() method
      */
-    "webhook_route_name" => "integrations.mlipa.webhook",
+    'webhook_route_name' => 'integrations.mlipa.webhook',
 
     /**
      * Payout route
      *
      * The route exposed in your application that will be used to verify payouts
      */
-    "payout_verification_route" => "integrations/mlipa/payouts/verification",
+    'payout_verification_route' => 'integrations/mlipa/payouts/verification',
 
     /**
      * When set to false errors will be thrown when they occur and you will have to
@@ -39,81 +39,80 @@ return [
      * will be silently handled and false status will be returned. The error can be found
      * in the logs.
      */
-    "handle_errors" => false,
+    'handle_errors' => false,
 
     /**
      * Payout route name
      *
      * The route name used for payout verification when using route() method
      */
-    "payout_verification_route_name" => "integrations.mlipa.payouts.verification",
+    'payout_verification_route_name' => 'integrations.mlipa.payouts.verification',
 
     /**
      * The root URL of the M-lipa requests
      */
-    "root_url" => "https://developer.mlipa.co.tz",
+    'root_url' => 'https://developer.mlipa.co.tz',
 
     /**
      * Endpoints for transactions
      */
-    "endpoints" => [
+    'endpoints' => [
 
         /**
          * Authentication endpoint
          *
          * The endpoint used for requesting the session token
          */
-        "authentication" => "/v2/auth/token",
+        'authentication' => '/v2/auth/token',
 
         /**
          * PushUSSD endpoint
          *
          * The endpoint used for initiating PushUSSD
          */
-        "pushussd" => "/v2/pushussd/create",
+        'pushussd' => '/v2/pushussd/create',
 
         /**
          * Billing endpoint
          *
          * The endpoint used for initiating billing transaction
          */
-        "billing" => "/v2/billings/create",
+        'billing' => '/v2/billings/create',
 
         /**
          * Payout endpoint
          *
          * The endpoint used for initiating a payout request
          */
-        "payout" => "/v2/payouts/create",
+        'payout' => '/v2/payouts/create',
 
         /**
          * Collection reconcilliation endpoint
          *
          * The endpoint used for collection reconcilliation
          */
-        "collection_reconcilliation" => "/v2/reconciliation/collection",
+        'collection_reconcilliation' => '/v2/reconciliation/collection',
 
         /**
          * Payout reconcilliation endpoint
          *
          * The endpoint used for payout reconcilliation
          */
-        "payout_reconcilliation" => "/v2/reconciliation/payout",
+        'payout_reconcilliation' => '/v2/reconciliation/payout',
     ],
 
     /**
      * Default heades sent to M-Lipa
-     *
      */
-    "default_headers" => [
+    'default_headers' => [
         /**
          * Accept application/json response from M-Lipa
          */
-        "Accept" => "application/json",
+        'Accept' => 'application/json',
 
         /**
          * The content type we are sending is application/json
          */
-        "Content-Type" => "application/json"
-    ]
+        'Content-Type' => 'application/json',
+    ],
 ];
--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 // webhook route
 Route::post(
-    config("mlipa.webhook_route"),
-    [MlipaController::class, "processWebhook"]
-)->name(config("mlipa.webhook_route_name"));
+    config('mlipa.webhook_route'),
+    [MlipaController::class, 'processWebhook']
+)->name(config('mlipa.webhook_route_name'));
 
 Route::any(
-    config("mlipa.payout_verification_route"),
-    [MlipaController::class, "verifyPayout"]
-)->name(config("mlipa.payout_verification_route_name"));
+    config('mlipa.payout_verification_route'),
+    [MlipaController::class, 'verifyPayout']
+)->name(config('mlipa.payout_verification_route_name'));
