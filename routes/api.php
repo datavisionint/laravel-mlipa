49a37a154b0aefe6a68b9fac3ebd0ace4dcea7d3
John Mwakalinga <mwakalingajohn@gmail.com>
2023-09-20 11:58:07 +0300

    add webhook routes

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,15 @@
 <?php
+
+use DatavisionInt\Mlipa\Http\Controllers\MlipaController;
+use Illuminate\Support\Facades\Route;
+
+// webhook route
+Route::post(
+    config("mlipa.webhook_route"),
+    [MlipaController::class, "processWebhook"]
+)->name(config("mlipa.webhook_route_name"));
+
+Route::any(
+    config("mlipa.payout_verification_route"),
+    [MlipaController::class, "verifyPayout"]
+)->name(config("mlipa.payout_verification_route_name"));
---
+++ src/Http/Controllers/MlipaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DatavisionInt\Mlipa\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MlipaController extends Controller
+{
+    public function processWebhook(Request $request)
+    {
+
+    }
+
+    public function verifyPayout(Request $request)
+    {
+
+    }
+}
