f1d528987945b3eb817d6cd7d54584c988d88597
John Mwakalinga <mwakalingajohn@gmail.com>
2023-09-22 08:51:24 +0300

    add collections table

--- src/MlipaServiceProvider.php
+++ src/MlipaServiceProvider.php
@@ -21,7 +21,9 @@ class MlipaServiceProvider extends PackageServiceProvider
             ->hasConfigFile()
             ->hasMigrations([
                 '2023_09_20_064740_create_mlipa_request_logs_table',
-                '2023_09_20_064741_create_mlipa_webhook_logs_table'
+                '2023_09_20_064741_create_mlipa_webhook_logs_table',
+                '2023_09_21_164503_create_payouts_table',
+                '2023_09_21_123451_create_collections_table'
             ])
             ->hasRoute('api');
     }
