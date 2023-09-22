9807bfe1c0dbb2f6f4a133c8b6abe85b1c8b2e6e
John Mwakalinga <mwakalingajohn@gmail.com>
2023-09-22 11:31:46 +0300

    Fix: Expose Mlipa facade methods to code completion

--- src/Facades/Mlipa.php
+++ src/Facades/Mlipa.php
@@ -5,6 +5,12 @@ namespace DatavisionInt\Mlipa\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static DatavisionInt\Mlipa\Mlipa initiatePushUssd(float $amount, string $msisdn, ?string $reference = null, string $nonce = null, string $currency = 'TZS'),
+ * @method static DatavisionInt\Mlipa\Mlipa initiateBilling(float $amount, string $msisdn, ?string $reference = null, string $nonce = null, string $currency = 'TZS'),
+ * @method static DatavisionInt\Mlipa\Mlipa initiatePayout(float $amount, string $msisdn, string $name = '', string $reference = null, string $nonce = null, string $currency = 'TZS'),
+ * @method static DatavisionInt\Mlipa\Mlipa reconcileCollection(string $reference = null),
+ * @method static DatavisionInt\Mlipa\Mlipa reconcilePayout(string $reference = null),
+ *
  * @see \DatavisionInt\Mlipa\Mlipa
  */
 class Mlipa extends Facade
